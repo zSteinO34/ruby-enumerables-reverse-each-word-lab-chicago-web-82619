acf6630902206a0a409d7fcfa28adb145235eb05
Zac Steinberg <zstein1234@gmail.com>
2019-08-15 23:57:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,12 +7,3 @@ def reverse_each_word(str)
   }
   result.join(" ")
 end
-
-def reverse_each_word(string)
-  array = string.split(" ") #turn string into an array
-  test_array = []
-  array.collect do|string| #iterate over the array
-    test_array << string.reverse #reverse each word in the array
-  end
-  test_array.join(" ")
-end
